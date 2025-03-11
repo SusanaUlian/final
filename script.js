2e9731b1561670a7b33bd119a1476d0da3b95e69
alexrod94 <alexrod94@gmail.com>
2025-03-11 15:31:37 -0300

    Cambios clase

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-async function getApiInfo () {
-    const res = await fetch("https://api.openbrewerydb.org/v1/breweries/?per_page=3");
-    const breweries = await res.json();
+async function getApiInfo() {
+  const res = await fetch(
+    "https://api.openbrewerydb.org/v1/breweries/?per_page=3"
+  );
+  const breweries = await res.json();
 
-    const results = breweries.map((brewery) => {
-        return {
-            name: brewery.name,
-            address: `${brewery.address_1}, ${brewery.city}, ${brewery.state}`,
-            phone: brewery.phone,
-        }
-    })
-    const projects = document.querySelector(".container-projects") ;
+  const results = breweries.map((brewery) => {
+    return {
+      name: brewery.name,
+      address: `${brewery.address_1}, ${brewery.city}, ${brewery.state}`,
+      phone: brewery.phone,
+    };
+  });
+  const projects = document.querySelector(".container-projects");
+  projects.innerHTML = "";
 
-    results.forEach((brewery) => {
-        projects.innerHTML += `<div class="container-projects">
+  results.forEach((brewery) => {
+    projects.innerHTML += `<div class="project">
+                        <img src="${"#"}" width="300" />
                         <h2 class="post-title">${brewery.name}</h2>
                         <p class="post-address">${brewery.address}</p>
                         <p class="post-phone">${brewery.phone}</p>
-                     </div>` 
-    })
+                     </div>`;
+  });
 }
 
 getApiInfo();
-
-
